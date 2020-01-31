9ffbbdd0a44996cdcebc8a819d29cded79363454
AndreiL26 <zcabatl@ucl.ac.uk>
2020-01-31 15:45:01 +0000

    Initial commit

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Slider slider;
+
+    // video player properties
+    private bool hasFinished;
+    
+    public bool IsPlaying {
+        get { return videoPlayer.isPlaying; }
+    }
+ 
+    public bool IsLooping {
+       get { return videoPlayer.isLooping; }
+    }
+
+    public bool IsPrepared {
+        get { return videoPlayer.isPrepared; }
+    }
+
+    public bool HasFinished {
+        get { return hasFinished; }
+    }
+
+    public double Time {
+        get { return videoPlayer.time; }
+    }
+
+    public ulong Duration {
+        get { return (ulong)(videoPlayer.frameCount/ videoPlayer.frameRate); }
+    }
+
+    public double NTime {
+        get { return Time / Duration; }
+    }
+
+    private void OnEnable() {
+        videoPlayer.errorReceived += errorReceived;
+        videoPlayer.frameReady += frameReady;
+        videoPlayer.loopPointReached += loopPointReached;
+        videoPlayer.prepareCompleted += prepareCompleted;
+        videoPlayer.seekCompleted += seekCompleted;
+        videoPlayer.started += started;
+    }
+
+    private void OnDisable() {
+        videoPlayer.errorReceived -= errorReceived;
+        videoPlayer.frameReady -= frameReady;
+        videoPlayer.loopPointReached -= loopPointReached;
+        videoPlayer.prepareCompleted -= prepareCompleted;
+        videoPlayer.seekCompleted -= seekCompleted;
+        videoPlayer.started -= started;
+    }
+
+    private void errorReceived(VideoPlayer videoPlayer, string message) {
+        Debug.Log("Video Player error: " + message);
+    }
+
+    private void frameReady(VideoPlayer videoPlayer, long frame) {
+        // Invoked every time a frame is ready -> CPU Costly
+    }
+
+    private void loopPointReached(VideoPlayer videoPlayer) {
+        Debug.Log("Video Player loop point reached");
+        hasFinished = true;
+    }
+
+    private void prepareCompleted(VideoPlayer videoPlayer) {
+        Debug.Log("Video Player finished preparing");
+        hasFinished = false;
+    }
+
+    private void seekCompleted(VideoPlayer videoPlayer) {
+        Debug.Log("Video Player finished seeking");
+        hasFinished = false;
+    }
+    
+    private void started(VideoPlayer videoPlayer) {
+        Debug.Log("Video Player started");
+    }
+
+    private void Update() {
+        if (IsPrepared) {
+            slider.value = (float)NTime;
+        }
+    }
+
+    public void LoadVideo(string name) {
+        string temp = Application.dataPath + "/StreamingAssets/" + name; /*.mp4,.avi,.mov*/
+
+    }
+
+    public void PlayVideo() {
+
+    }
+
+    public void PauseVideo() {
+
+    }
+
+    public void RestardVideo() {
+
+    }
+
+    public void LoopVideo(bool toggle) {
+
+    }
+
+    public void Seek(float nTime) {
+
+    }
+
+    public void IncremenetPlaybackSpeed() {
+
+    }
+
+    public void DecrementPlaybackSpeed() {
+
+    }
+
+
+ 
+}
---
+++ Assets/Scripts/VideoProgressBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoProgressBar : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    private Slider slider;
+
+    void Awake()
+    {
+        videoPlayer = GameObject.Find("360 Video Player").GetComponent<VideoPlayer>();
+        slider = GetComponent<Slider>();   
+    }
+
+    void Update()
+    {
+        if (videoPlayer.frameCount > 0) {
+            slider.value = (float)videoPlayer.frame / (float)videoPlayer.frameCount;
+        }
+    }
+
+}
